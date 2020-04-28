2a20f53352131e76839c4ed837121249e56ac4af
jrana12 <62606186+jrana12@users.noreply.github.com>
2020-04-28 12:24:32 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/style.css" type="text/css">
 
     <title>Sign-Up</title>
