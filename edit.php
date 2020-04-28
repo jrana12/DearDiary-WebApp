79d3e88a77a43bcacab11f495dba583b8eb493ea
jrana12 <62606186+jrana12@users.noreply.github.com>
2020-04-28 12:22:57 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="css/profilestyle.css" >
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>Add Note!</title>
